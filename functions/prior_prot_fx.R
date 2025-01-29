e6424edca351aa9cfc2e29739eedf1181cf44bec
Juliano Palacios Abrantes <j.palacios@oceans.ubc.ca>
2025-01-29 18:09:42 -0300

    Moved prioritization for protected cells to a function

---
+++ functions/prior_prot_fx.R
@@ -0,0 +1,15 @@
+# Deal with duplicates, prioritize 'protected' status if there are multiple statuses for the same index
+
+prior_prot <- function(data){
+  
+non_duplicated_data <- data %>% 
+  group_by(index) %>%
+  # Create a flag column to mark protected status rows (if any) for each index
+  mutate(priority_status = if_else(status == "protected", 1, 0)) %>%
+  arrange(index, desc(priority_status)) %>%  # Ensure 'protected' rows come first for each index
+  # Keep only the first occurrence for each index, effectively keeping 'protected' if it exists
+  distinct(index, .keep_all = TRUE) %>%
+  ungroup()
+  
+return(non_duplicated_data)
+}
