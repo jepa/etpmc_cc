fd04e3dae2f14ae080a246e791c3020304ce1a8e
Juliano Palacios Abrantes <j.palacios@oceans.ubc.ca>
2025-03-29 12:51:26 -0700

    add bar and map options

--- functions/hist_dif_fx.R
+++ functions/hist_dif_fx.R
@@ -37,53 +37,53 @@ hist_dif <- function(category, var, map = T, bar = T){
   
   
   if(map == T){
-  for(i in 1:2){
-    
-    ssps <- c("ssp585","ssp126")[i]
-    
-    suppressMessages(
-      suppressWarnings(
-        # species map
-        map_t <- ggplot() +
-          geom_sf(data = sau_sf %>% select(region = name) %>%  left_join(spp_data) %>% filter(ssp == ssps),
-                  aes(fill = dif_2040),
-                  color = "grey"
-          ) +
-          geom_sf(data = cmar_sf %>%  left_join(spp_data) %>% filter(ssp == ssps),
-                  aes(fill = dif_2040),
-                  color = "black"
-          ) +
-          facet_wrap(~scenario) +
-        scale_fill_gradient2("Change in value relative to hisotric (%)") +
-          # ggtitle(etpmc_spp %>% filter(taxon_key == taxon) %>% pull(common_name)) +
-          # ggtitle(paste(category,var)) +
-          my_ggtheme_m("Reg",facet_tl_s = 6,ax_tl_s = 6,ax_tx_s = 4,leg_tl_s = 4,leg_tx_s = 4) +
-          theme(legend.text = element_text(size = 6),
-                legend.title = element_text(size = 6),
-                legend.key.height = unit(0.2, "line"),
-                title = element_text(size = 6)
-          )
-        
+    for(i in 1:2){
+      
+      ssps <- c("ssp585","ssp126")[i]
+      
+      suppressMessages(
+        suppressWarnings(
+          # species map
+          map_t <- ggplot() +
+            geom_sf(data = sau_sf %>% select(region = name) %>%  left_join(spp_data) %>% filter(ssp == ssps),
+                    aes(fill = dif_2040),
+                    color = "grey"
+            ) +
+            geom_sf(data = cmar_sf %>%  left_join(spp_data) %>% filter(ssp == ssps),
+                    aes(fill = dif_2040),
+                    color = "black"
+            ) +
+            facet_wrap(~scenario) +
+            scale_fill_gradient2("Change in value relative to hisotric (%)") +
+            # ggtitle(etpmc_spp %>% filter(taxon_key == taxon) %>% pull(common_name)) +
+            # ggtitle(paste(category,var)) +
+            my_ggtheme_m("Reg",facet_tl_s = 6,ax_tl_s = 6,ax_tx_s = 4,leg_tl_s = 4,leg_tx_s = 4) +
+            theme(legend.text = element_text(size = 6),
+                  legend.title = element_text(size = 6),
+                  legend.key.height = unit(0.2, "line"),
+                  title = element_text(size = 6)
+            )
+          
+        )
       )
-    )
-    map_name <- paste0("map_",unique(spp_data$category),"_",var,"_",ssps,"_scen_diff.png")
-    
-    if(is.numeric(category) == F){
-      ggsave(
-        my_path("R","figures/relative_to_hist/groups/",map_name),
-        map_t,
-        height = 4,
-        width = 5)
-    }else{
-      ggsave(
-        my_path("R","figures/relative_to_hist/species/",map_name),
-        map_t,
-        height = 4,
-        width = 5)
+      map_name <- paste0("map_",unique(spp_data$category),"_",var,"_",ssps,"_scen_diff.png")
+      
+      if(is.numeric(category) == F){
+        ggsave(
+          my_path("R","figures/relative_to_hist/groups/",map_name),
+          map_t,
+          height = 4,
+          width = 5)
+      }else{
+        ggsave(
+          my_path("R","figures/relative_to_hist/species/",map_name),
+          map_t,
+          height = 4,
+          width = 5)
+      }
+      
     }
-    
   }
-}
   
   if(bar == T){
     
@@ -91,46 +91,46 @@ hist_dif <- function(category, var, map = T, bar = T){
       
       ssps <- c("ssp585","ssp126")[i]
       
-    spp_data %>% 
-      filter(ssp == ssps,
-             variable == var) %>% 
-      mutate(region = gsub("_"," ",region)) %>% 
-      ggplot() + 
-      geom_bar(
-        aes(
-          x = region,
-          y = dif_2040,
-          fill = scenario
-        ),
-        stat = "identity",
-        position = "dodge"
-      ) +
-      # facet_grid(~period) +
-      scale_fill_viridis_d() +
-      coord_flip() +
-      labs(x="",
-           y = "Change in value relative to historic (%)")+
-      my_ggtheme_p(leg_pos = "right") +
-      scale_x_discrete(limits = rev(sort(spp_data %>% mutate(region = gsub("_"," ",region)) %>% pull(region) %>% unique())))
-    
-    
-    bar_name <- paste0("bar_",unique(spp_data$category),"_",var,"_scen_diff_",ssps,".png")
-    
-    
-    if(is.numeric(category) == F){
-      ggsave(
-        my_path("R","figures/relative_to_hist/groups/",bar_name),
-        last_plot(),
-        height = 4,
-        width = 7)
-    }else{
-      ggsave(
-        my_path("R","figures/relative_to_hist/species/",bar_name),
-        last_plot(),
-        height = 4,
-        width = 7)
-    }
-    
+      spp_data %>% 
+        filter(ssp == ssps,
+               variable == var) %>% 
+        mutate(region = gsub("_"," ",region)) %>% 
+        ggplot() + 
+        geom_bar(
+          aes(
+            x = region,
+            y = dif_2040,
+            fill = scenario
+          ),
+          stat = "identity",
+          position = "dodge"
+        ) +
+        # facet_grid(~period) +
+        scale_fill_viridis_d() +
+        coord_flip() +
+        labs(x="",
+             y = "Change in value relative to historic (%)")+
+        my_ggtheme_p(leg_pos = "right") +
+        scale_x_discrete(limits = rev(sort(spp_data %>% mutate(region = gsub("_"," ",region)) %>% pull(region) %>% unique())))
+      
+      
+      bar_name <- paste0("bar_",unique(spp_data$category),"_",var,"_scen_diff_",ssps,".png")
+      
+      
+      if(is.numeric(category) == F){
+        ggsave(
+          my_path("R","figures/relative_to_hist/groups/",bar_name),
+          last_plot(),
+          height = 4,
+          width = 7)
+      }else{
+        ggsave(
+          my_path("R","figures/relative_to_hist/species/",bar_name),
+          last_plot(),
+          height = 4,
+          width = 7)
+      }
+      
     }
   }
   
--- functions/scen_dif_fx.R
+++ functions/scen_dif_fx.R
@@ -1,15 +1,11 @@
 
 #Function to create maps of differences between scenarios relative to the SQ scenario
 
-scen_dif_fx <- function(category, var = "abd", ssp_selected = "ssp585"){
+scen_dif_fx <- function(category, var = "abd", ssp_selected = "ssp585", map = T, bar = T){
   
   print(category)
   
-  if(var == "abd"){
-    name <- paste0(category,"_",var,"_period.csv")
-  }else{
-    name <- paste0(category,"_",var,"_period.csv")
-  }
+  name <- paste0(category,"_",var,"_period.csv")
   
   if(is.numeric(category) == F){
     spp_data <- my_path("R","processed/groups/period_data/",name,read = T) %>% 
@@ -41,44 +37,90 @@ scen_dif_fx <- function(category, var = "abd", ssp_selected = "ssp585"){
       per_change = ifelse(per_change == "Inf",100,per_change)
     )
   
-  
-  # species map
-  map_t <- ggplot() +
-    geom_sf(data = sau_sf %>% select(region = name) %>%  left_join(spp_data) %>% filter(ssp == ssp_selected),
-            aes(fill = per_change),
-            color = "grey"
-    ) +
-    geom_sf(data = cmar_sf %>%  left_join(spp_data) %>% filter(ssp == ssp_selected),
-            aes(fill = per_change),
-            color = "black"
-    ) +
-    # facet_grid(period~scen) +
-    facet_wrap(~scenario) +
-    scale_fill_gradient2("Percentage change relative to the SQ scenario by 2040") +
-    # ggtitle(etpmc_spp %>% filter(taxon_key == taxon) %>% pull(common_name)) +
-    # ggtitle(paste(category,var)) +
-    my_ggtheme_m("Reg",facet_tl_s = 6,ax_tl_s = 6,ax_tx_s = 4,leg_tl_s = 4,leg_tx_s = 4) +
-    theme(legend.text = element_text(size = 6),
-          legend.title = element_text(size = 6),
-          legend.key.height = unit(0.2, "line"),
-          title = element_text(size = 6)
-    )
-  
-  
-  map_name <- paste0(unique(spp_data$category),"_",var,"_",ssp_selected,"_scen_diff.png")
-  
-  if(is.numeric(category) == F){
-    ggsave(
-      my_path("R","figures/scen_diff/groups/",map_name),
-      map_t,
-      height = 4,
-      width = 5)
-  }else{
-    ggsave(
-      my_path("R","figures/scen_diff/species/",map_name),
-      map_t,
-      height = 4,
-      width = 5)
+  if(map == T){
+    # species map
+    map_t <- ggplot() +
+      geom_sf(data = sau_sf %>% select(region = name) %>%  left_join(spp_data) %>% filter(ssp == ssp_selected),
+              aes(fill = per_change),
+              color = "grey"
+      ) +
+      geom_sf(data = cmar_sf %>%  left_join(spp_data) %>% filter(ssp == ssp_selected),
+              aes(fill = per_change),
+              color = "black"
+      ) +
+      # facet_grid(period~scen) +
+      facet_wrap(~scenario) +
+      scale_fill_gradient2("Percentage change relative to the SQ scenario by 2040") +
+      # ggtitle(etpmc_spp %>% filter(taxon_key == taxon) %>% pull(common_name)) +
+      # ggtitle(paste(category,var)) +
+      my_ggtheme_m("Reg",facet_tl_s = 6,ax_tl_s = 6,ax_tx_s = 4,leg_tl_s = 4,leg_tx_s = 4) +
+      theme(legend.text = element_text(size = 6),
+            legend.title = element_text(size = 6),
+            legend.key.height = unit(0.2, "line"),
+            title = element_text(size = 6)
+      )
+    
+    
+    map_name <- paste0(unique(spp_data$category),"_",var,"_",ssp_selected,"_scen_diff.png")
+    
+    if(is.numeric(category) == F){
+      ggsave(
+        my_path("R","figures/scen_diff/groups/",map_name),
+        map_t,
+        height = 4,
+        width = 5)
+    }else{
+      ggsave(
+        my_path("R","figures/scen_diff/species/",map_name),
+        map_t,
+        height = 4,
+        width = 5)
+    }
+    
+  }
+  if(bar == T){
+    
+    
+    spp_data %>% 
+      filter(ssp == ssp_selected) %>% 
+      mutate(region = gsub("_"," ",region)) %>% 
+      ggplot() + 
+      geom_bar(
+        aes(
+          x = region,
+          y = per_change,
+          fill = scenario
+        ),
+        stat = "identity",
+        position = "dodge"
+      ) +
+      # facet_grid(~period) +
+      scale_fill_viridis_d() +
+      coord_flip() +
+      labs(x="",
+           y = "Change in value relative to historic (%)")+
+      my_ggtheme_p(leg_pos = "right") +
+      scale_x_discrete(limits = rev(sort(spp_data %>% mutate(region = gsub("_"," ",region)) %>% pull(region) %>% unique())))
+    
+    
+    bar_name <- paste0("bar_",unique(spp_data$category),"_",var,"_scen_diff_",ssps,".png")
+    
+    
+    if(is.numeric(category) == F){
+      ggsave(
+        my_path("R","figures/scen_diff/groups/",bar_name),
+        last_plot(),
+        height = 4,
+        width = 7)
+    }else{
+      ggsave(
+        my_path("R","figures/scen_diff/species/",bar_name),
+        last_plot(),
+        height = 4,
+        width = 7)
+    }
+    
+    
   }
   
   
