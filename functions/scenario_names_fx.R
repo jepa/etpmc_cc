da684e5e62ed1ba2f8bb543ff9ba6a43071590c6
Juliano Palacios Abrantes <j.palacios@oceans.ubc.ca>
2025-07-29 22:26:37 -0300

    included in spanish for report

--- functions/scenario_names_fx.R
+++ functions/scenario_names_fx.R
@@ -1,6 +1,20 @@
 # This function simply translate the scenario names for figures
-scenario_names <- function(data){
+scenario_names <- function(data, leng = "spa"){
   
+  if(leng == "spa"){
+  data_clean <- data %>% 
+    mutate(
+      scenario = ifelse(str_detect(scen,"nr"),"Regulaciones no implementadas",
+                        ifelse(str_detect(scen,"rc"),"Regulaciones de conservación",
+                               ifelse(str_detect(scen,"ri"),"Regulaciones implementadas",
+                                      ifelse(str_detect(scen,"rp"),"Regulaciones de pesca",
+                                             ifelse(str_detect(scen,"sq"),"Status quo",scen)
+                               )
+                               )
+                        )
+      )
+    )
+  }else{
   data_clean <- data %>% 
     mutate(
       scenario = ifelse(str_detect(scen,"nr"),"Regulations not implemented",
@@ -8,11 +22,12 @@ scenario_names <- function(data){
                                ifelse(str_detect(scen,"ri"),"Regulations implemented",
                                       ifelse(str_detect(scen,"rp"),"Regulations for fishing",
                                              ifelse(str_detect(scen,"sq"),"Status quo",scen)
-                               )
+                                      )
                                )
                         )
       )
     )
+  }
   
   return(data_clean)
 }
